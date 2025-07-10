9a3cd933539d2f4f33ef54376d87b32c1296db8c
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 19:51:13 +0900

    Update scale_color_comp.R

--- R/scale_color_comp.R
+++ R/scale_color_comp.R
@@ -1,7 +1,16 @@
-#' ggplot2 color scale with complementary palette
+#' ggplot2 color scale with customizable complementary palette
 #'
-#' @param ... Arguments passed to comp_palette and scale_color_manual
+#' @param start_color Optional HEX color for starting hue
+#' @param recycle Whether to recycle if needed
+#' @param c Chroma
+#' @param l Lightness
+#' @param ... Other arguments to pass to discrete_scale
 #' @export
-scale_color_comp <- function(...) {
-  ggplot2::discrete_scale("colour", "comp", comp_palette, ...)
+scale_color_comp <- function(start_color = NULL, recycle = TRUE, c = 100, l = 65, ...) {
+  ggplot2::discrete_scale(
+    "colour",
+    "comp",
+    palette = function(n) comp_palette(n, start_color = start_color, recycle = recycle, c = c, l = l),
+    ...
+  )
 }
