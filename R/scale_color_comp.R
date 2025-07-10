96dc4516dca7619a74e7d251f408040216efefdc
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 21:26:24 +0900

    Update scale_color_comp.R

--- R/scale_color_comp.R
+++ R/scale_color_comp.R
@@ -8,7 +8,7 @@
 scale_color_comp <- function(start = NULL, recycle = TRUE, div = NULL, ...) {
   ggplot2::discrete_scale(
     "colour", "comp",
-    palette = function(n) comp_palette(n, start = start, recycle = recycle, div = div),
+    palette = function(n) comp_palette(start = start, recycle = recycle, div = div),
     ...
   )
 }
