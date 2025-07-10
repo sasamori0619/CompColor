47d9700b498f2c2df0023ef50c9a3442d0993209
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 19:51:03 +0900

    Update scale_fill_comp.R

--- R/scale_fill_comp.R
+++ R/scale_fill_comp.R
@@ -1,7 +1,16 @@
-#' ggplot2 fill scale with complementary palette
+#' ggplot2 fill scale with customizable complementary palette
 #'
-#' @param ... Arguments passed to comp_palette and scale_fill_manual
+#' @param start_color Optional HEX color for starting hue
+#' @param recycle Whether to recycle if needed
+#' @param c Chroma
+#' @param l Lightness
+#' @param ... Other arguments to pass to discrete_scale
 #' @export
-scale_fill_comp <- function(...) {
-  ggplot2::discrete_scale("fill", "comp", comp_palette, ...)
+scale_fill_comp <- function(start_color = NULL, recycle = TRUE, c = 100, l = 65, ...) {
+  ggplot2::discrete_scale(
+    "fill",
+    "comp",
+    palette = function(n) comp_palette(n, start_color = start_color, recycle = recycle, c = c, l = l),
+    ...
+  )
 }
