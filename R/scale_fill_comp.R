37f3919a17bdffbf8cc312c439252ca946c8abbc
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 20:34:50 +0900

    Update scale_fill_comp.R

--- R/scale_fill_comp.R
+++ R/scale_fill_comp.R
@@ -3,14 +3,14 @@
 #' @param start Optional HEX color
 #' @param recycle Whether to recycle colors if needed
 #' @param div Number of hue divisions (overrides n-based split)
-#' @param c Chroma (used only if start_color is NULL)
-#' @param l Lightness (used only if start_color is NULL)
+#' @param c_val Chroma (used only if start_color is NULL)
+#' @param l_val Lightness (used only if start_color is NULL)
 #' @param ... Passed to scale_fill_manual
 #' @export
-scale_fill_comp <- function(start = NULL, recycle = TRUE, div = NULL, c = 100, l = 65, ...) {
+scale_fill_comp <- function(start = NULL, recycle = TRUE, div = NULL, ...) {
   ggplot2::discrete_scale(
     "fill", "comp",
-    palette = function(n) comp_palette(n, start = start, recycle = recycle, div = div, c = c, l = l),
+    palette = function(n) comp_palette(n, start = start, recycle = recycle, div = div),
     ...
   )
 }
