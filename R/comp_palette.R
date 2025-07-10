fac162ade67d17fb54225317933d0942cfa0cddd
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 21:35:35 +0900

    Update comp_palette.R

--- R/comp_palette.R
+++ R/comp_palette.R
@@ -28,7 +28,7 @@ comp_palette <- function(n, start = NULL, div = 8) {
   }
 
   # === Hue の分割 ===
-  k <- div
+  k <- if (!is.null(div)) div else 8
   base_hues <- (h_start + seq(0, length.out = k, by = 360 / k)) %% 360
 
   # === インデックスの並べ替え（指定の順序）===
