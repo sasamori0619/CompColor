f4f565c7a993899870022c12e4076d747abe7ba5
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-10 20:35:19 +0900

    Update scale_fill_comp.R

--- R/scale_fill_comp.R
+++ R/scale_fill_comp.R
@@ -3,8 +3,6 @@
 #' @param start Optional HEX color
 #' @param recycle Whether to recycle colors if needed
 #' @param div Number of hue divisions (overrides n-based split)
-#' @param c_val Chroma (used only if start_color is NULL)
-#' @param l_val Lightness (used only if start_color is NULL)
 #' @param ... Passed to scale_fill_manual
 #' @export
 scale_fill_comp <- function(start = NULL, recycle = TRUE, div = NULL, ...) {
