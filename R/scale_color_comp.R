eb7906e0c8c30bb6ab6c41e6d51b2e1759d5bee5
sasamori0619 <62057885+sasamori0619@users.noreply.github.com>
2025-07-11 15:12:52 +0900

    Update scale_color_comp.R

--- R/scale_color_comp.R
+++ R/scale_color_comp.R
@@ -4,10 +4,10 @@
 #' @param div Number of hue divisions (overrides n-based split)
 #' @param ... Passed to scale_fill_manual
 #' @export
-scale_color_comp <- function(start = NULL, div = NULL, ...) {
+scale_color_comp <- function(start = NULL, div = NULL, skip = NULL, ...) {
   ggplot2::discrete_scale(
     "colour", "comp",
-    palette = function(n) comp_palette(n, start = start,  div = div),
+    palette = function(n) comp_palette(n, start = start,  div = div skip = skip),
     ...
   )
 }
